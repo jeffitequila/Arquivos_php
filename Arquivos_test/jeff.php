80138375754b7f54519755bebdbb0833d9131d9c
jeffitequila <43590568+jeffitequila@users.noreply.github.com>
2018-10-25 15:23:39 -0300

    Novos arquivos

---
+++ Arquivos_test/jeff.php
@@ -0,0 +1,17 @@
+<?php
+//tag
+    $variavel1 = 100;
+    $variavel2 = 200;
+
+    $num1 = 10;
+    $num2 = 20;
+
+        echo "$num1<br>";
+        echo "$num2<br>";
+        echo "$variavel1<br>";
+        echo "$variavel2<br>";
+
+
+echo "Essa frase foi escrita através da função echo<br>";
+echo "Essa é outra linha escrita pela função echo";
+
